82152810e8a2f26d29ddc4cc3ab634f079938e9d
fosc <foscento@gmail.com>
2021-02-01 21:34:02 -0500

    gets display working

--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+from time import sleep
 from copy import deepcopy
 import random
 from collections import Counter
@@ -5,10 +6,10 @@ import tcod as libtcod
 
 
 class CellGrid:
-    def __init__(self, width, height):
+    def __init__(self, width, height, density):
         self.height = height
         self.width = width
-        self.cells = [[int(random.random() + 0.5) for x in range(width)] for y in range(height)]
+        self.cells = [[int(random.random() + density) for x in range(width)] for y in range(height)]
 
     def get_cell(self, x, y):
         try:
@@ -24,11 +25,11 @@ class CellGrid:
 
 
 class CellularAutomaton:
-    def __init__(self, width, height, edge_default=1):
+    def __init__(self, width, height, density=0.5, edge_default=1):
         self.edge_default = edge_default
-        self.cells = CellGrid(width, height)
+        self.cells = CellGrid(width, height, density=density)
 
-    def next_generation(self, neighbor_counts):
+    def next_generation(self, neighbor_counts: dict, state:int) -> int:
         raise NotImplementedError("Abstract method: subclass must implement.")
 
     def get_neighborhood(self, x, y):
@@ -47,7 +48,7 @@ class CellularAutomaton:
         return Counter(self.get_neighborhood(x, y))
 
     def next_state(self, x, y):
-        return self.next_generation(self.neighbor_profile(x, y))
+        return self.next_generation(self.neighbor_profile(x, y), self.cells.get_cell(x, y))
 
     def advance_state(self):
         next_state = deepcopy(self.cells)
@@ -57,32 +58,62 @@ class CellularAutomaton:
         self.cells = next_state
 
 
+class TestAutomaton(CellularAutomaton):
+    def next_generation(self, neighbor_counts, state):
+        if neighbor_counts[1] > 4:
+            return 1
+        else:
+            return 0
+
+
+class ConwayAutomaton(CellularAutomaton):
+    def next_generation(self, neighbor_counts, state):
+        if state == 1 and neighbor_counts[1] in (2,3):
+            return 1
+        if state == 0 and neighbor_counts[1] == 3:
+            return 1
+        return 0
+
 class AutomatonObserver:
+
+    tileset = libtcod.tileset.load_tilesheet(
+        "img.png", 32, 8, libtcod.tileset.CHARMAP_TCOD,
+    )
     def __init__(self, state, mapping):
         self.mapping = mapping
-        self.state = state
-        self.con = libtcod.console_new(self.state.width, self.state.height)
-        libtcod.console_init_root(self.state.width, self.state.height, 'libtcod tutorial', False)
+        self.refresh = True
+        self._state = state
+        self.pix_size =3
+        self.context = libtcod.context.new_window(width=self.pix_size*self.state.width,
+                                                  height=self.pix_size*self.state.height,
+                                                  tileset=self.tileset)
+        self.con = self.context.new_console( min_columns=self.state.width,
+                                            min_rows=self.state.height,
+                                            magnification=0.9)
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, new_state):
+        self._state = new_state
         self.show()
 
     def show(self):
-        key = libtcod.Key()
-        mouse = libtcod.Mouse()
-        while not libtcod.console_is_window_closed():
-            for y in range(self.state.height):
-                for x in range(self.state.width):
-                    color = self.mapping[self.state.get_cell(x, y)]
-                    libtcod.console_set_char_background(self.con, x, y, color, libtcod.BKGND_SET)
-
-            libtcod.console_blit(self.con, 0, 0, self.con.width, self.con.height, 0, 0, 0)
-            libtcod.console_flush()  # present everything on screen
-
-            libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
-            if key.vk == libtcod.KEY_ESCAPE:
-                return True
+        self.con.clear()
+        for y in range(self.state.height):
+            for x in range(self.state.width):
+                color = self.mapping[self.state.get_cell(x, y)]
+                self.con.print(x, y, ' ', bg=color)
+        self.context.present(self.con)
 
 
 if __name__ == '__main__':
-    mapping = {0: libtcod.white, 1: libtcod.black}
-    aut = CellularAutomaton(50, 50)
+    mapping = {0: (255,255,255), 1:(0,0,0)}
+    aut = TestAutomaton(500, 500, edge_default=1, density=0.6)
     observer = AutomatonObserver(aut.cells, mapping)
+    for i in range(10):
+        aut.advance_state()
+        observer.state = aut.cells
+        input("hit enter")
