58fb1df68b52aeba52b512dcbd9cb269c2407180
fosc <foscento@gmail.com>
2021-02-11 17:43:55 -0500

    refactor into MapBuilder class

--- flaskr/make_noise.py
+++ flaskr/make_noise.py
@@ -1,43 +1,16 @@
 """Create a JSON string with the to_json function. This string is a instance of Map Data"""
 import json
-from random import randint
+from configparser import ConfigParser
 from noise import pnoise2
 
 OCEAN_BLUE = '#028bad'
-
-
-def get_noise_value(n_x, n_y, freq, base):
-    """Return a float between 0 and 1 of perlin noise"""
-    raw = pnoise2(freq*n_x, freq*n_y, repeatx=freq+1, repeaty=freq+1, base=base)  # in [-1, 1]
-    return (raw + 1) / 2  # between 0 and 1
-
-
-def coord_transform(x, max_x):
-    """Transform coordinate x in [0, max_x] to a value in [-1, 1]"""
-    return (x/max_x - 0.5)*2
-
-
-def noise_grid(width, height, freq, dec_places=2) -> list:
-    """Return a list of lists representing a grid of perlin noise"""
-    profile = [1, 2, 4]
-    base = randint(0, 20)  # we don't want the same thing every time
-    result = list()
-    for y in range(height):
-        result.append(list())
-        for x in range(width):
-            n_x = coord_transform(x, width)
-            n_y = coord_transform(y, height)
-            n_val = 0
-            for mode in profile:
-                n_val += (1/mode)*get_noise_value(n_x, n_y, mode*freq, base)
-            n_val = n_val/sum(map(lambda i: i**-1, profile))
-            n_val = round(n_val, dec_places)
-            result[y].append(n_val)
-    return result
+CONFIG_FILE = '../config.ini'
+CONFIG_READER = ConfigParser()
+CONFIG_READER.read(CONFIG_FILE)
 
 
 def hex_string(int_val):
-    """Return a string of length 2 representing the hex value of int_value"""
+    """return a string of length 2 representing the hex value of int_value"""
     str_val = hex(int_val).replace('0x', '')
     if len(str_val) < 2:
         return '0' + str_val
@@ -45,12 +18,18 @@ def hex_string(int_val):
 
 
 def css_greyscale(int_val):
-    """Given an integer return a greyscale css color string."""
+    """given an integer return a greyscale css color string."""
     return '#'+hex_string(int_val)*3
 
 
-def color_mapping(num_dec_place, water_level=0.473):
-    resolution = 10**num_dec_place
+def color_mapping(config):
+    try:
+        dec_places = int(CONFIG_READER[config]['decimal_places'])
+        water_level = float(CONFIG_READER[config]['water_level'])
+    except KeyError:
+        raise Exception(f'{CONFIG_FILE} section {config} is missing required value(s).')
+
+    resolution = 10**dec_places
     shades = [x/resolution for x in range(resolution + 1)]
     mapping = dict()
     for shade in shades:
@@ -61,16 +40,71 @@ def color_mapping(num_dec_place, water_level=0.473):
     return mapping
 
 
-def to_json(width, height, freq, dec_places=3):
-    grid = noise_grid(width, height, freq, dec_places)
-    dump_dict = {'num_rows': height,
-                 'num_cols': width,
-                 'color_map': color_mapping(dec_places),
-                 'data': list()}
-    for y in range(height):
-        for x in range(width):
-            dump_dict['data'].append({'x': x, 'y': y, 'val': str(grid[y][x])})
-    return json.dumps(dump_dict)
+class Grid:
+    def __init__(self, width, height):
+        self._grid = list()
+        for y in range(height):
+            self._grid.append(list())
+            for x in range(width):
+                self._grid[y].append(0)
+        self.width = width
+        self.height = height
 
-if __name__ == "__main__":
-    print(color_mapping(2))
+    def set(self, x, y, val):
+        self._grid[y][x] = val
+
+    def get(self, x, y):
+        return self._grid[y][x]
+
+    def to_dict(self):
+        dump_dict = {'num_rows': self.height, 'num_cols': self.width, 'data': list()}
+        for y in range(self.height):
+            for x in range(self.width):
+                dump_dict['data'].append({'x': x, 'y': y, 'val': str(self.get(x, y))})
+        return dump_dict
+
+
+class NoiseGrid(Grid):
+    def __init__(self, width, height, config='Default'):
+        super().__init__(width, height)
+        try:
+            self.freq = float(CONFIG_READER[config]['frequency'])
+            self.profile = json.loads(CONFIG_READER[config]['profile'])
+            self.base = int(CONFIG_READER[config]['base'])
+            self.dec_places = int(CONFIG_READER[config]['decimal_places'])
+        except KeyError:
+            raise Exception(f'{CONFIG_FILE} section {config} is missing required value(s).')
+
+        for y in range(self.height):
+            for x in range(self.width):
+                self.set_layered_noise_value(x, y)
+
+    def get_raw_noise_value(self, n_x, n_y, freq):
+        """Return a float between 0 and 1 of perlin noise"""
+        raw = pnoise2(freq*n_x, freq*n_y, repeatx=freq+1, repeaty=freq+1, base=self.base)
+        return (raw + 1) / 2  # convert [-1, 1] -> [0, 1]
+
+    def set_layered_noise_value(self, x, y):
+        """Set grid value at x, y to a value of layered frequencies of perlin noise"""
+        def coord_transform(i, max_i):
+            """Transform coordinate i in [0, max_i] to a value in [-1, 1]"""
+            return (i / max_i - 0.5) * 2
+        n_x = coord_transform(x, self.width)
+        n_y = coord_transform(y, self.height)
+        n_val = 0
+        for mode in self.profile:
+            n_val += (1 / mode) * self.get_raw_noise_value(n_x, n_y, mode * self.freq)
+        n_val = n_val / sum(map(lambda i: i ** -1, self.profile))
+        n_val = round(n_val, self.dec_places)
+        self.set(x, y, n_val)
+
+
+class MapBuilder:
+    def __init__(self, width, height, config='Default'):
+        self.noise_grid = NoiseGrid(width, height, config)
+        self.color_map = color_mapping(config)
+
+    def map_data(self):
+        dump_dict = self.noise_grid.to_dict()
+        dump_dict['color_map'] = self.color_map
+        return json.dumps(dump_dict)
